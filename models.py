c93804d569cff23b14a8d8b0e658daa1439273bb
desk <qudwndh@gmail.com>
2024-06-28 17:02:38 +0900

    login

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
